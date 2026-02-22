3ab382949964db365cae6cda1b0855a1e6a4924c
singilwon <164003006+singilwon@users.noreply.github.com>
2026-02-22 22:47:36 +0900

    Title: 두수의 곱과 몫, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 260222/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
+++ 260222/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -4,5 +4,5 @@ let [a,b] = fs.readFileSync(0).toString().trim().split(" ");
 a = Number(a);
 b = Number(b);
 
-console.log(`300 * 5 = ${a*b}`);
-console.log(`300 / 5 = ${a/b}`);
+console.log(`${a} * ${b} = ${a*b}`);
+console.log(`${a} / ${b} = ${parseInt(a/b)}`);
