3adae9f24df6cdb20860e2764e9f30f41e8f5ed1
singilwon <164003006+singilwon@users.noreply.github.com>
2026-02-23 15:22:07 +0900

    Title: 시력 검사 2, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 260223/시력 검사 2/eye-test-2.js
+++ 260223/시력 검사 2/eye-test-2.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ");
 let a = Number(input[0]);
 
-if(a>1.0) console.log("High")
-else if (a>0.5) console.log("Middle")
+if(a>=1.0) console.log("High")
+else if (a>=0.5) console.log("Middle")
 else console.log("Low")
