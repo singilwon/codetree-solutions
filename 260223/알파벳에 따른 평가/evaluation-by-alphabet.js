f49ff9d043bfa2d2b8b947743ee9e8937458cea1
singilwon <164003006+singilwon@users.noreply.github.com>
2026-02-23 15:27:59 +0900

    Title: 알파벳에 따른 평가, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260223/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+
+if(a==="S") console.log("Superior");
+else if(a==="A") console.log("Excellenet");
+else if(a==="B") console.log("Good");
+else if(a==="C") console.log("Usually");
+else console.log("Effort")
