58b5429c58b41c37cf9d57e493ab7837e0766f95
singilwon <164003006+singilwon@users.noreply.github.com>
2026-02-23 15:30:23 +0900

    Title: 살 수 있는 물건 2, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260223/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+
+if(a>=3000) console.log("book");
+else if(a>=1000) console.log("mask");
+else if(a>=500) console.log("pen");
+else console.log("no")
