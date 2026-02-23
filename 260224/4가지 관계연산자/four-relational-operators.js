86ec7a669117bf66217fbc1294844edd4df4f316
singilwon <164003006+singilwon@users.noreply.github.com>
2026-02-24 00:49:23 +0900

    Title: 4가지 관계연산자, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260224/4가지 관계연산자/four-relational-operators.js
@@ -0,0 +1,15 @@
+let fs = require("fs");
+let [a,b] = fs.readFileSync(0).toString().trim().split(" ");
+
+a = Number(a);
+b = Number(b);
+
+if(a>=b) console.log(1);
+else console.log(0);
+if(a>b) console.log(1);
+else console.log(0);
+if(a<=b) console.log(1);
+else console.log(0);
+if(a<b) console.log(1);
+else console.log(0);
+
