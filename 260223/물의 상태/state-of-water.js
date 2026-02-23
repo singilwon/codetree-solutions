2fec1445dd205c1c715dd377cfaf27bb98fa4920
singilwon <164003006+singilwon@users.noreply.github.com>
2026-02-23 15:20:49 +0900

    Title: 물의 상태, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 260223/물의 상태/state-of-water.js
+++ 260223/물의 상태/state-of-water.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ");
 let a = Number(input[0]);
 
-if(a<=0) console.log("ice")
+if(a<0) console.log("ice")
 else if(a>=100) console.log("vapor");
 else console.log("water")
