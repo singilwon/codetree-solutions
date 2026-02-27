b81d5470fd233568e3d051ab80f2e47c11e88e14
singilwon <164003006+singilwon@users.noreply.github.com>
2026-02-27 11:05:29 +0900

    Title: 최댓값 구하기, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260227/최댓값 구하기/maximum-value.js
+++ 260227/최댓값 구하기/maximum-value.js
@@ -4,4 +4,4 @@ a = Number(a);
 b = Number(b);
 c = Number(c);
 
-a>b&&a>c?console.log(a) : b>a&&b>c?console.log(b):console.log(c)
+a>=b&&a>=c?console.log(a) : b>=a&&b>=c?console.log(b):console.log(c)
