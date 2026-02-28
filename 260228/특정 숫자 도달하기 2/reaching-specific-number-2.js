15c26cd022a313150ffe2a9043ea586c7b872921
singilwon <164003006+singilwon@users.noreply.github.com>
2026-02-28 15:57:14 +0900

    Title: 특정 숫자 도달하기 2, Time: 73ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260228/특정 숫자 도달하기 2/reaching-specific-number-2.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString();
+input = Number(input);
+let answer = input;
+
+for(let i = input-1;i>=1;i--) {
+    answer += " " + i;
+    console.log(i);
+}
+console.log(answer);
