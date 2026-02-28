d5e01c37eda63597b8f0a14757a2eac70074c83e
singilwon <164003006+singilwon@users.noreply.github.com>
2026-02-28 15:51:31 +0900

    Title: 홀수만 출력, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 260228/홀수만 출력/output-only-odd.js
+++ 260228/홀수만 출력/output-only-odd.js
@@ -1,13 +1,11 @@
 const fs = require("fs");
 let [a,b] = fs.readFileSync(0).toString().trim().split(" ");
-let answer = a;
 a = Number(a);
 b = Number(b);
 
-
+let answer = a;
 
 for(let i = a+2;i<=b;i+=2) {
-    console.log(i);
-    answer = " " + i;
+    answer = answer + " " + i;
 }
 console.log(answer);
