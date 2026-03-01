7e964231ec93daccfae69ff0dfb858dbca2589d2
singilwon <164003006+singilwon@users.noreply.github.com>
2026-03-01 20:47:54 +0900

    Title: a로 채워넣기, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

--- 260301/a로 채워넣기/filling-with-a.js
+++ 260301/a로 채워넣기/filling-with-a.js
@@ -3,6 +3,6 @@ let a = fs.readFileSync(0).toString();
 
 a = a.split("");
 a[1] = "a";
-if(a[a.length-1]==="\n") a.shift();
+if(a[a.length-1]==="\n") a.pop();
 a[a.length-2] = "a";
 console.log(a.join(""));
