c6500593cc38922bf039bdb45eeb2cced0e7881e
singilwon <164003006+singilwon@users.noreply.github.com>
2026-03-01 20:04:21 +0900

    Title: 제곱하여 출력하기, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260301/제곱하여 출력하기/print-square-of-elements.js
+++ 260301/제곱하여 출력하기/print-square-of-elements.js
@@ -1,1 +1,10 @@
+const fs = require("fs");
+let [length,arr] = fs.readFileSync(0).toString().trim().split("\n");
 
+arr = arr.split(" ");
+const answer = [];
+for(let i = 0;i<length;i++) {
+    answer.push(Number(arr[i]) * Number(arr[i]));
+}
+
+console.log(answer.join(" "));
