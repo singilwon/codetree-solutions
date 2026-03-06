11776d619521176542d8342848f4cd0eac035e5c
singilwon <164003006+singilwon@users.noreply.github.com>
2026-03-06 23:28:27 +0900

    Title: 정상적인 구구단, Time: 87ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260306/정상적인 구구단/normal-multiple.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = Number(fs.readFileSync(0).toString());
+
+for(let i = 1;i<=input;i++) {
+    let answer = []
+    for (let j = 1;j<=input;j++) {
+        answer.push(`${i} * ${j} = ${i*j}`);
+    }
+    console.log(answer.join(", "))
+}
