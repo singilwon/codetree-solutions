345b01b0efb2349735bce5a9d214e87eee06b2ef
singilwon <164003006+singilwon@users.noreply.github.com>
2026-03-07 15:57:25 +0900

    Title: 두 개의 격자 비교하기, Time: 73ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260307/두 개의 격자 비교하기/compare-two-grid.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let length = input.shift().split(" ").map(Number);
+let A = input.slice(0,length[0]).map(v => v.split(" ").map(Number));
+let B = input.slice(length[0],length[0]*2).map(v => v.split(" ").map(Number));
+
+for(let i = 0; i < length[0]; i++) {
+    console.log(A[i].map((v,j) => {
+        if(v === B[i][j]) return 0
+        else return 1
+        } ).join(" "));
+}
