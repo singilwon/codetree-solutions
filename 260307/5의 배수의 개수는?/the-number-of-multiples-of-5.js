1f7ae2bfb8094d2dbe8c7539701a9e7454a4afb7
singilwon <164003006+singilwon@users.noreply.github.com>
2026-03-07 16:35:08 +0900

    Title: 5의 배수의 개수는?, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 260307/5의 배수의 개수는?/the-number-of-multiples-of-5.js
+++ 260307/5의 배수의 개수는?/the-number-of-multiples-of-5.js
@@ -2,8 +2,9 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 let answer = 0;
 for(let i = 0;i<4;i++) {
+    let arr = input[i].trim().split(" ").map(Number);
     for(let j = 0;j<4;j++) {
-        if(Number(input[i][j]) % 5===0) answer++;
+        if(arr[j] % 5===0) answer++;
         }   
 } 
 console.log(answer)
