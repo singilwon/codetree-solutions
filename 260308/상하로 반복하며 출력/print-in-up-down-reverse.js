94276cfac4a930d3a9176a8282804823286dc8d1
singilwon <164003006+singilwon@users.noreply.github.com>
2026-03-08 16:26:19 +0900

    Title: 상하로 반복하며 출력, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260308/상하로 반복하며 출력/print-in-up-down-reverse.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = Number(fs.readFileSync(0).toString());
+
+
+for(let i = 1 ; i <= input ; i++) {
+    const arr = []
+    for(let j = 1 ; j <= input ; j++) {
+        if((j+1)%2===0) arr.push(i);
+        else arr.push(Math.abs((input+1)-i));
+    }
+    console.log(arr.join(""));
+} 
