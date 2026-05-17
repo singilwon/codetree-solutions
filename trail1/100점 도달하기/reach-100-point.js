900148ff2f92b0ce769cacc8cc9b7a5b04c30fdb
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-17 17:52:45 +0900

    [Passed] 100점 도달하기 (66ms, 10MB)

---
+++ trail1/100점 도달하기/reach-100-point.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let N = Number(fs.readFileSync(0).toString().trim());
+
+let answer = "";
+
+for (let i = N; i <= 100; i++) {
+    if (i >= 90) answer += "A ";
+    else if (i >= 80) answer += "B ";
+    else if (i >= 70) answer += "C ";
+    else if (i >= 60) answer += "D ";
+    else answer += "F "
+}
+
+console.log(answer);
