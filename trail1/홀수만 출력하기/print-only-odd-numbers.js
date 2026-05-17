fb45231b96630accc2629ffa3d6c37c1788267ec
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-17 18:15:11 +0900

    [Passed] 홀수만 출력하기 (64ms, 10MB)

--- trail1/홀수만 출력하기/print-only-odd-numbers.js
+++ trail1/홀수만 출력하기/print-only-odd-numbers.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
-let arr = fs.readFileSync(0).toString().trim().split("\n").map(Number);
+const arr = fs.readFileSync(0).toString().trim().split("\n").map(Number);
+
+arr.shift();
 
 for (const v of arr) {
     if (v % 2 !== 0 && v % 3 === 0) console.log(v);
