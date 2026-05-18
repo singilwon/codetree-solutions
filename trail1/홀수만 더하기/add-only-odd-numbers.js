7341b3fa35f1cd6020cd8fa16366f2db959b0eae
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-18 16:51:33 +0900

    [Wrong Answer] 홀수만 더하기 (66ms, 10MB)

---
+++ trail1/홀수만 더하기/add-only-odd-numbers.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const arr = fs.readFileSync(0).toString().trim().split("\n").map(Number);
+
+let hap = 0;
+
+for (const v of arr) {
+    if (v % 2 !== 0 && v % 3 === 0) hap += v;
+}
+
+console.log(hap)
