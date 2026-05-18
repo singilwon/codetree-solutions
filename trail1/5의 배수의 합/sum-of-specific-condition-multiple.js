7f39b37d2c57a103167af8de5716ba333868ae63
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-18 16:59:06 +0900

    [Passed] 5의 배수의 합 (69ms, 10MB)

--- trail1/5의 배수의 합/sum-of-specific-condition-multiple.js
+++ trail1/5의 배수의 합/sum-of-specific-condition-multiple.js
@@ -2,10 +2,21 @@ const fs = require("fs");
 const [A, B] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
 
-let hap = 0;
 
-for (let i = A; i <= B; i++) {
-    if (i % 5 === 0 || i % 7 === 0) {
-        hap += i;
+let hap = 0;
+if (B > A) {
+    for (let i = A; i <= B; i++) {
+        if (i % 5 === 0) {
+            hap += i;
+        }
+    }
+} else {
+    for (let i = B; i <= A; i++) {
+        if (i % 5 === 0) {
+            hap += i;
+        }
     }
 }
+
+
+console.log(hap)
