80e3e98f4ff2a0b6784f65d183412a54a7f47931
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-19 22:56:26 +0900

    [Passed] 입력받은 수의 합과 평균 (68ms, 13MB)

--- trail1/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.js
+++ trail1/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.js
@@ -4,9 +4,7 @@ const [N, ...arr] = fs.readFileSync(0).toString().trim().split("\n").map(Number)
 let sum = 0;
 
 for (const v of arr) {
-    if (v >= 0 && v <= 200) {
-        sum += v;
-    }
+    sum += v;
 }
 
 console.log(sum + " " + (sum / N).toFixed(1));
