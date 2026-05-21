3ffb2cccf36b21e78f0e0b145aae0c4729160162
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-21 23:07:38 +0900

    [Wrong Answer] A의 배수들의 곱 (62ms, 10MB)

--- trail1/A의 배수들의 곱/product-of-multiples-of-a.js
+++ trail1/A의 배수들의 곱/product-of-multiples-of-a.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const [A, B] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-let mu = 1;
+let mul = 1;
 
 for (let i = 1; i <= B; i++) {
     if (i % A === 0) mul *= i;
