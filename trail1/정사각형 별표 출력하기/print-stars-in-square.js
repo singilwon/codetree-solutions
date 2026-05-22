540d7e646c42d050d94fa0f229599b0497abf886
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-22 16:59:33 +0900

    [Passed] 정사각형 별표 출력하기 (70ms, 14MB)

---
+++ trail1/정사각형 별표 출력하기/print-stars-in-square.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const N = Number(fs.readFileSync(0).toString().trim());
+
+for (let i = 0; i < N; i++) {
+    let str = "";
+    for (let j = 0; j < N; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
