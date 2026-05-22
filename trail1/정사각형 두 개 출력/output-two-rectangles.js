5cd7741decabd81ebcacc23ec2248500777f92e1
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-22 17:13:22 +0900

    [Passed] 정사각형 두 개 출력 (63ms, 10MB)

---
+++ trail1/정사각형 두 개 출력/output-two-rectangles.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const N = Number(fs.readFileSync(0).toString().trim());
+
+for (let i = 0; i < N; i++) {
+    let str = "";
+    for (let j = 0; j < N; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
+console.log(" ")
+
+for (let i = 0; i < N; i++) {
+    let str = "";
+    for (let j = 0; j < N; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
