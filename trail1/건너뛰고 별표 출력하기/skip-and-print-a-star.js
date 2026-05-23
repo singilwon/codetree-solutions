e9d17cae442c9fc1939a70a2c62154c504b53d5b
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-23 15:28:06 +0900

    [Passed] 건너뛰고 별표 출력하기 (73ms, 24MB)

---
+++ trail1/건너뛰고 별표 출력하기/skip-and-print-a-star.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const N = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+for (let i = N; i >= 1; i--) {
+    str = "";
+    for (let j = 0; j <= N - i; j++) {
+        str += "*";
+    }
+    console.log(str);
+    console.log(" ");
+}
+
+for (let i = 1; i < N; i++) {
+    str = "";
+    for (let j = 0; j < N - i; j++) {
+        str += "*";
+    }
+    console.log(str);
+    console.log(" ");
+}
+
+
