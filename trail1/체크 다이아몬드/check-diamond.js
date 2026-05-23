3748cfe36ba1c19ff9bed6534644981c475a5a75
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-23 16:40:11 +0900

    [Passed] 체크 다이아몬드 (62ms, 10MB)

---
+++ trail1/체크 다이아몬드/check-diamond.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const N = Number(fs.readFileSync(0).toString().trim());
+
+
+for (let i = 1; i <= N; i++) {
+    let str = ""
+
+    for (let k = 1; k <= N - i; k++) {
+        str += " ";
+    }
+
+    for (let j = 1; j <= i; j++) {
+        str += "*" + " ";
+    }
+    console.log(str);
+}
+
+for (let i = N - 1; i >= 1; i--) {
+    let str = ""
+
+    for (let k = 1; k <= N - i; k++) {
+        str += " ";
+    }
+
+    for (let j = 1; j <= i; j++) {
+        str += "*" + " ";
+    }
+    console.log(str);
+}
