affe6c59bad2ed7d982b5875b3e81d3736c613bb
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-24 19:14:26 +0900

    [Passed] 1부터의 합 (63ms, 10MB)

---
+++ trail1/1부터의 합/sum-from-the-one.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const N = Number(fs.readFileSync(0).toString().trim());
+
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+    sum += i;
+    if (sum >= N) {
+        console.log(i);
+        break;
+    }
+}
