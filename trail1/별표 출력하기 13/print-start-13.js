10c20e7e1e31403c6179b6260b69bc72184c286d
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-25 14:08:33 +0900

    [Passed] 별표 출력하기 13 (65ms, 10MB)

---
+++ trail1/별표 출력하기 13/print-start-13.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const N = Number(fs.readFileSync(0).toString().trim());
+
+const counts = [];
+
+let first = 1;
+let last = N;
+
+for (let i = 1; i <= N; i++) {
+    if (i % 2 === 0) {
+        counts.push(first);
+        first++;
+    } else {
+        counts.push(last);
+        last--;
+    }
+}
+
+for (let i = counts.length - 1; i >= 0; i--) {
+    counts.push(counts[i]);
+}
+
+for (const count of counts) {
+    console.log("* ".repeat(count));
+}
