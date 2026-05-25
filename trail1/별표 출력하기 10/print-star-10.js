eca4a1b3781ffef785cd59137e059f62627210b2
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-25 13:54:59 +0900

    [Wrong Answer] 별표 출력하기 10 (82ms, 21MB)

--- trail1/별표 출력하기 10/print-star-10.js
+++ trail1/별표 출력하기 10/print-star-10.js
@@ -4,39 +4,78 @@ const N = Number(fs.readFileSync(0).toString().trim());
 let first = 1;
 let last = N;
 
-for (let i = 1; i <= N; i++) {
-    let str = "";
-    if (i % 2 === 0) {
-        for (let j = 0; j < last; j++) {
-            str += "*" + " ";
+if (N % 2 !== 0) {
+    for (let i = 1; i <= N; i++) {
+        let str = "";
+        if (i % 2 === 0) {
+            for (let j = 0; j < last; j++) {
+                str += "*" + " ";
+            }
+            last--;
+            console.log(str);
+        } else {
+            for (let j = 0; j < first; j++) {
+                str += "*" + " ";
+            }
+            first++;
+            console.log(str);
         }
-        last--;
-        console.log(str);
-    } else {
-        for (let j = 0; j < first; j++) {
-            str += "*" + " ";
-        }
-        first++;
-        console.log(str);
     }
-}
 
-last++;
-first--;
+    last++;
+    first--;
 
-for (let i = 1; i <= N; i++) {
-    let str = "";
-    if (i % 2 === 0) {
-        for (let j = 0; j < last; j++) {
-            str += "*" + " ";
+    for (let i = 1; i <= N; i++) {
+        let str = "";
+        if (i % 2 === 0) {
+            for (let j = 0; j < last; j++) {
+                str += "*" + " ";
+            }
+            last++;
+            console.log(str);
+        } else {
+            for (let j = 0; j < first; j++) {
+                str += "*" + " ";
+            }
+            first--;
+            console.log(str);
         }
-        last++;
-        console.log(str);
-    } else {
-        for (let j = 0; j < first; j++) {
-            str += "*" + " ";
+    }
+} else {
+    for (let i = 1; i <= N; i++) {
+        let str = "";
+        if (i % 2 === 0) {
+            for (let j = 0; j < last; j++) {
+                str += "*" + " ";
+            }
+            last--;
+            console.log(str);
+        } else {
+            for (let j = 0; j < first; j++) {
+                str += "*" + " ";
+            }
+            first++;
+            console.log(str);
         }
-        first--;
-        console.log(str);
+    }
+
+    last++;
+    first--;
+
+    for (let i = 1; i <= N; i++) {
+        let str = "";
+        if (i % 2 === 0) {
+            for (let j = 0; j < first; j++) {
+                str += "*" + " ";
+            }
+            first++;
+            console.log(str);
+        } else {
+            for (let j = 0; j < last; j++) {
+                str += "*" + " ";
+            }
+            last--;
+            console.log(str);
+        }
     }
 }
