8dceaaebf91f78c726ccb48750f60f428f75ebac
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-30 13:48:20 +0900

    [Passed] 좌우 지그재그 출력 (64ms, 11MB)

--- trail1/좌우 지그재그 출력/print-lr-zigzag.js
+++ trail1/좌우 지그재그 출력/print-lr-zigzag.js
@@ -7,13 +7,15 @@ let even = N * 2;
 for (let i = 0; i < N; i++) {
     let str = "";
     for (let j = 1; j <= N; j++) {
-        if (i % 2 !== 0) {
-            str += odd;
+        if (i % 2 === 0) {
+            str += odd + " ";
             odd++;
         } else {
-            str += even;
+            str += even + " ";
             even--;
         }
     }
+    if (i % 2 === 0) even = odd + N - 1;
+    else odd = even + N + 1;
     console.log(str);
 }
