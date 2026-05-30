9370390ccfcc1baaf9cfd5e1523116c828643dd2
singilwon <164003006+singilwon@users.noreply.github.com>
2026-05-30 19:01:48 +0900

    [Passed] 숫자 피라미드 3 (65ms, 11MB)

---
+++ trail1/숫자 피라미드 3/number-pyramid-3.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const N = Number(fs.readFileSync(0).toString().trim());
+
+for (let i = 1; i <= N; i++) {
+    let str = "";
+    for (let j = 0; j < i; j++) {
+        str += (i + (i * j)) + " ";
+    }
+    console.log(str)
+}
