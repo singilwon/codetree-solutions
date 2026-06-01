d6e7c3774361092da596a59576c5ce76b7a3813d
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-01 15:45:18 +0900

    [Wrong Answer] 특이한 출력 (67ms, 10MB)

---
+++ trail1/특이한 출력/special-print.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const N = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+for (let i = 1; i <= N; i++) {
+
+    for (let j = 1; j <= N; j++) {
+        str += `(${i}, ${j}) `;
+        if (i + j === 4) {
+            console.log(str);
+            str = "";
+        }
+    }
+}
+console.log(str);
