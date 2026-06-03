de3d1ee846596b703465122cd894936d183d9356
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-03 15:20:30 +0900

    [Passed] 3N + 1 수열 M번 (64ms, 10MB)

--- trail1/3N + 1 수열 M번/3n-plus-1-sequence-m-times.js
+++ trail1/3N + 1 수열 M번/3n-plus-1-sequence-m-times.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 let [N, ...arr] = fs.readFileSync(0).toString().trim().split("\n");
 
-let cnt = 0;
 
 for (let i = 0; i < N; i++) {
     let temp = Number(arr[i]);
+    let cnt = 0;
     while (temp !== 1) {
-        if (temp % 2 === 0) temp = Math.floor(temp / 2);
+        if (temp % 2 === 0) temp /= 2;
         else temp = (3 * temp) + 1;
         cnt++;
     }
