d39f1d133bba939cac9d2793297d54e6a0c9b927
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-03 15:34:34 +0900

    [Passed] 약수가 세 개인 수 (69ms, 14MB)

---
+++ trail1/약수가 세 개인 수/numbers-with-three-divisors.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let cnt = 0;
+
+for (let i = a; i <= b; i++) {
+    let num = 0;
+    for (let j = 1; j <= i; j++) {
+        if (i % j === 0) num++;
+    }
+    if (num === 3) cnt++;
+}
+
+console.log(cnt);
