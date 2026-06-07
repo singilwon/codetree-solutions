45d47b16849bec61c6f3b8f5901f1a2ab3e7d8e0
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-07 14:24:30 +0900

    [Passed] 격자로 사각형 만들기 (61ms, 10MB)

--- trail1/격자로 사각형 만들기/print-grid-in-rectangle.js
+++ trail1/격자로 사각형 만들기/print-grid-in-rectangle.js
@@ -4,8 +4,8 @@ const N = Number(fs.readFileSync(0).toString().trim());
 
 const answer = Array(N).fill(1).map(() => Array(N).fill(1));
 
-for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 5; j++) {
+for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
         if (j === 0 || i === 0) continue;
         answer[i][j] = answer[i][j - 1] + answer[i - 1][j] + answer[i - 1][j - 1];
     }
