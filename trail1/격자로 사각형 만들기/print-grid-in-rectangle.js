ec49dcb642854c25555dd9b45c97e4ccfba8af93
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-07 14:23:51 +0900

    [Runtime Error] 격자로 사각형 만들기 (66ms, 10MB)

---
+++ trail1/격자로 사각형 만들기/print-grid-in-rectangle.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const N = Number(fs.readFileSync(0).toString().trim());
+
+const answer = Array(N).fill(1).map(() => Array(N).fill(1));
+
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        if (j === 0 || i === 0) continue;
+        answer[i][j] = answer[i][j - 1] + answer[i - 1][j] + answer[i - 1][j - 1];
+    }
+}
+
+
+console.log(answer.map(row => row.join(" ")).join("\n"));
