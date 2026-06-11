2c0ae0a6b96dfa89a13dd503fa03f61bae9bde4c
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-11 14:21:06 +0900

    [Passed] 함수를 이용해 직사각형 만들기 (65ms, 10MB)

---
+++ trail2/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+let [n, m] = input[0].split(" ").map(Number);
+
+function printRect(n, m) {
+    for (let i = 0; i < n; i++) {
+        let str = "";
+        for (let j = 0; j < m; j++) {
+            str += "1";
+        }
+        console.log(str);
+    }
+}
+
+printRect(n,m)
