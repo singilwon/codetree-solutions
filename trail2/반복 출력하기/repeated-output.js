7dae7e5b20c695b7d9c7ae305d0ed085105bbe28
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-11 14:19:43 +0900

    [Passed] 반복 출력하기 (66ms, 10MB)

---
+++ trail2/반복 출력하기/repeated-output.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+
+function Numbers(n) {
+    for (let i = 0; i < n; i++) {
+        console.log("12345^&*()_")
+    }
+}
+
+
+Numbers(n);
