3b6e06b967ac871b573c5afa2f76dc4a508813bd
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-12 15:36:51 +0900

    [Passed] 1부터 특정 수까지의 합 (65ms, 10MB)

---
+++ trail2/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+
+function Hap(n) {
+    let answer = 0;
+    for (let i = 1; i <= n; i++) answer += i;
+    console.log(Math.trunc(answer / 10));
+}
+
+Hap(n);
