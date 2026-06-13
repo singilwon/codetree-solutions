9d8a7831ce9cbc264307df23f825133b3f3604f1
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-13 22:48:15 +0900

    [Wrong Answer] 정수의 최솟값 (64ms, 10MB)

---
+++ trail2/정수의 최솟값/minimum-value-of-an-integer.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [a, b, c] = input[0].split(" ").map(Number);
+
+function Min(a, b, c) {
+    let min = 0;
+    a > b ? b > c ? min = c : min = b : a < c ? min = a : min = 0;
+    return min;
+}
+
+console.log(Min(a, b, c));
