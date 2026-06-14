9a4604f0f351dfa033c959c4b0eae9ed9f68976b
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-14 14:45:08 +0900

    [Passed] 짝수이면서 합이 5의 배수인 수 (85ms, 30MB)

--- trail2/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.js
+++ trail2/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.js
@@ -5,7 +5,7 @@ const n = Number(input[0]);
 function YesOrNo(n) {
 
     if (n % 2 === 0 && ((Math.trunc(n / 10) + n % 10) % 5 === 0)) return "Yes";
-    else return "No;"
+    else return "No";
 }
 
 console.log(YesOrNo(n));
