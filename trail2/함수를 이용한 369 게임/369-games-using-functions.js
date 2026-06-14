91d484349643815d070734838c371e7c0855d850
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-14 22:08:58 +0900

    [Passed] 함수를 이용한 369 게임 (148ms, 17MB)

--- trail2/함수를 이용한 369 게임/369-games-using-functions.js
+++ trail2/함수를 이용한 369 게임/369-games-using-functions.js
@@ -2,18 +2,23 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const [A, B] = input[0].split(" ").map(Number);
 
-function YesOrNo(A, B) {
+let cnt = 0;
 
-    let cnt = 0;
-    for (let i = A; i <= B; i++) {
-        let ten = parseInt(i / 10);
-        let one = i % 10;
-        if (i % 3 === 0) cnt++;
-        else if (ten === 3 || ten === 6 || ten === 9) cnt++;
-        else if (one === 3 || one === 6 || one === 9) cnt++;
+function has369(num) {
+    const str = String(num);
+
+    for (const ch of str) {
+        if (ch === "3" || ch === "6" || ch === "9") {
+            return true;
+        } else if (num % 3 === 0) {
+            return true;
+        }
     }
-    return cnt;
+
+    return false;
+}
+for (let i = A; i <= B; i++) {
+    if (has369(i)) cnt++;
 
 }
-
-console.log(YesOrNo(A, B));
+console.log(cnt);
