cca68d5978d63617f942f95e51e37b6ac9be1b3f
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-15 01:18:34 +0900

    [Passed] 두 수의 거듭제곱 (62ms, 10MB)

---
+++ trail2/두 수의 거듭제곱/two-numbers-of-squares.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(" ");
+const [a, b] = input.map(Number);
+
+function square(a, b) {
+    let answer = 1;
+    for (let i = 0; i < b; i++) {
+        answer *= a;
+    }
+    return answer;
+}
+
+console.log(square(a, b));
