c2757211d0344fdf2b69bfabfb30cb213ab3b0d6
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-19 16:11:05 +0900

    [Passed] 반복 출력하기 2 (65ms, 10MB)

---
+++ trail2/반복 출력하기 2/repeated-output-2.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+
+function printWorld(n) {
+    if (n == 0) {
+        return;
+    }
+
+    printWorld(n - 1);
+    console.log("HelloWorld");
+}
+
+printWorld(n);
