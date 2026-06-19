82aaa6fc741597ad9795ca16880764fb2a808475
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-19 16:28:22 +0900

    [Passed] 재귀함수의 꽃 (67ms, 10MB)

--- trail2/재귀함수의 꽃/the-flower-of-the-recursive-function.js
+++ trail2/재귀함수의 꽃/the-flower-of-the-recursive-function.js
@@ -11,8 +11,8 @@ function f(a, t, l) {
     process.stdout.write(a[t] + " ");
 }
 
-let a = Array(5).fill(0);
-for (let i = 0; i < 5; i++) {
+let a = Array(n).fill(0);
+for (let i = 0; i < n; i++) {
     a[i] = i + 1;
 }
 f(a, n - 1, 0);
