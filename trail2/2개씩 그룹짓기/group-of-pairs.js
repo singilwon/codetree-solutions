09d542812ad67235af244026a461f3a708eac874
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-22 16:48:17 +0900

    [Wrong Answer] 2개씩 그룹짓기 (64ms, 10MB)

--- trail2/2개씩 그룹짓기/group-of-pairs.js
+++ trail2/2개씩 그룹짓기/group-of-pairs.js
@@ -7,7 +7,7 @@ const nums = input[1].trim().split(' ').map(Number);
 nums.sort((a, b) => a - b);
 
 
-if (nums[n - 1] + nums[n] > nums[2 * n - 1]) {
+if (nums[n - 1] + nums[n] > nums[2 * n - 1] + nums[0]) {
     console.log(nums[n - 1] + nums[n]);
 } else {
     console.log(nums[0] + nums[2 * n - 1]);
