38e803460caf4ded1112fe2c2ecbc53827e7c2ca
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-22 16:38:53 +0900

    [Passed] Top K 숫자 구하기 (65ms, 10MB)

--- trail2/Top K 숫자 구하기/kth-number.js
+++ trail2/Top K 숫자 구하기/kth-number.js
@@ -2,5 +2,5 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, k] = input[0].split(' ').map(Number);
 const nums = input[1].trim().split(' ').map(Number);
-nums.sort();
-console.log(nums[k - 1]);
+
+console.log(nums.sort((a, b) => a - b)[k - 1]);
