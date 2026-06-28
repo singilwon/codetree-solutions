ba00a32a67dbfdb4465c425f893d7e4a8ca6c0c2
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-28 16:21:55 +0900

    [Wrong Answer] Date to Date (78ms, 13MB)

--- trail2/Date to Date/date-to-date.js
+++ trail2/Date to Date/date-to-date.js
@@ -21,7 +21,8 @@ function cal(m, d) {
 let totalDay1 = cal(m1, d1);
 let totalDay2 = cal(m2, d2);
 
-console.log(totalDay2 - totalDay1);
+if (m1 === m2 && d1 === d2) console.log(1);
+else console.log(totalDay2 - totalDay1);
 
 
 
