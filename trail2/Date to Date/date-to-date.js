84968ad8d8353cfaa70216d2ce7e2e14b9ed1726
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-28 16:20:58 +0900

    [Wrong Answer] Date to Date (63ms, 10MB)

---
+++ trail2/Date to Date/date-to-date.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+
+const num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function cal(m, d) {
+    let answer = 0;
+    let month = 0;
+    while (true) {
+        if (m - 1 === month) break;
+        answer += num_of_days[month];
+        month++;
+
+    }
+    answer += d;
+    return answer;
+}
+
+let totalDay1 = cal(m1, d1);
+let totalDay2 = cal(m2, d2);
+
+console.log(totalDay2 - totalDay1);
+
+
+
+
+
+
