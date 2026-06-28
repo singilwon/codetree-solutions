ec945924f7db856a614b2d92d26873f47dc199fb
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-28 16:47:44 +0900

    [Wrong Answer] 요일 맞추기 (64ms, 10MB)

---
+++ trail2/요일 맞추기/guess-day-of-week.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+
+const day_of_week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function monthToday(m) {
+    let month = 1;
+    let answer = 0;
+    while (true) {
+        if (month === m) break;
+        answer += num_of_days[month];
+        month++;
+    }
+    return answer;
+}
+
+let day1 = monthToday(m1) + d1;
+let day2 = monthToday(m2) + d2;
+
+const gap = (day1 - day2) % 7;
+
+console.log(day_of_week[gap - 1]);
