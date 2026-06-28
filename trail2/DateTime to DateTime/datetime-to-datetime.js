8012d2531bcc5ec093b55fe6891644180cdb6cc5
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-28 16:36:49 +0900

    [Wrong Answer] DateTime to DateTime (68ms, 10MB)

---
+++ trail2/DateTime to DateTime/datetime-to-datetime.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [A, B, C] = input[0].split(' ').map(Number);
+
+let min = ((B * 60) + C) - ((11 * 60) + 11);
+
+let day = A - 11;
+
+let answer = (day * 24 * 60) + min;
+
+console.log(answer);
