24eb2355abbb316bd73b79f4d11f99d16217628b
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-29 15:00:46 +0900

    [Wrong Answer] 십진수로 변환하기 (74ms, 10MB)

---
+++ trail2/십진수로 변환하기/convert-to-decimal.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const binary = input[0].split("").map(Number);
+
+let num = 0;
+
+for (let i = 0; i < 5; i++) {
+    num = num * 2 + binary[i];
+}
+
+console.log(num);
