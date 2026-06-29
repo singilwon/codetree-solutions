13a7fe60ac85066ebbcf250e0d937746b84fe3c3
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-29 14:52:46 +0900

    [Passed] 2진수로 변환하기 (67ms, 11MB)

---
+++ trail2/2진수로 변환하기/convert-to-binary.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let n = Number(input[0]);
+
+let digits = [];
+
+while (true) {
+    if (n < 2) {
+        digits.push(n);
+        break;
+    }
+
+    digits.push(n % 2);
+    n = Math.floor(n / 2);
+}
+
+// 이진수 출력
+let binaryNumber = '';
+for (let i = digits.length - 1; i >= 0; i--) {
+    binaryNumber += digits[i];
+}
+
+console.log(binaryNumber);
+
