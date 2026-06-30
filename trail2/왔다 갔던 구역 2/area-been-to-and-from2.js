8f85a05f4fac07aa93947a61e6d7c2333728e772
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-30 15:29:21 +0900

    [Wrong Answer] 왔다 갔던 구역 2 (75ms, 10MB)

---
+++ trail2/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const commands = input.slice(1, n + 1);
+let current = 50;
+
+const visited = Array(100).fill(0);
+
+for (const command of commands) {
+    const [len, dir] = command.split(" ");
+    for (let i = 0; i < Number(len); i++) {
+        if (dir === "R") {
+            visited[current]++;
+            current++;
+        } else {
+            visited[current]++;
+            current--;
+        }
+    }
+}
+
+let answer = 0;
+for (const v of visited) {
+    if (v >= 2) answer++;
+}
+
+console.log(answer);
