32dfb780cc973f8c0d9f9d6e67fa4787977a167f
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-30 16:00:15 +0900

    [Wrong Answer] 흰검 칠하기 (62ms, 10MB)

---
+++ trail2/흰검 칠하기/painting-white-black.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const commands = input.slice(1).map(line => line.split(' '));
+
+const visited = Array(50).fill(0).map(() => Array(3).fill(0));
+
+let current = 25;
+
+for (const [len, dir] of commands) {
+    for (let i = 0; i < Number(len); i++) {
+        if (dir === "R") {
+            visited[current][0]++;
+            visited[current][2] = "B";
+            current++;
+
+        } else {
+            visited[current][1]++;
+            visited[current][2] = "W";
+            
+        }
+    }
+}
+
+let answer = [0, 0, 0];
+for (const [black, white, lastDir] of visited) {
+    if (black >= 2 && white >= 2) answer[2]++;
+    else if (lastDir === "B") answer[1]++;
+    else if (lastDir === "W") answer[0]++;
+}
+console.log(...answer);
