896b49bd0e56386f9f9b825dfcc087709945255f
singilwon <164003006+singilwon@users.noreply.github.com>
2026-06-30 15:07:23 +0900

    [Passed] 최대로 겹치는 지점 (75ms, 10MB)

---
+++ trail2/최대로 겹치는 지점/maximum-overlapped-points.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const segments = [];
+for (let i = 1; i <= n; i++) {
+    segments.push(input[i].split(' ').map(Number));
+}
+
+
+const visited = Array(100).fill(0);
+
+for (const [start, end] of segments) {
+    for (let i = start; i <= end; i++) {
+        visited[i]++;
+    }
+}
+
+let max = 0;
+for (const v of visited) {
+    if (max < v) max = v;
+}
+
+console.log(max);
