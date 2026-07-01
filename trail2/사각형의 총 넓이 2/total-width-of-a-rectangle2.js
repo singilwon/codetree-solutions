30b9bb4777556d9a26f5e874826e2a56f782295b
singilwon <164003006+singilwon@users.noreply.github.com>
2026-07-01 16:11:22 +0900

    [Runtime Error] 사각형의 총 넓이 2 (65ms, 10MB)

---
+++ trail2/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const rects = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+
+const area = Array(10).fill(0).map(() => Array(10).fill(0));
+
+for (let i = 0; i < n; i++) {
+    for (let j = rects[i][0]; j < rects[i][2]; j++) {
+
+        for (let k = rects[i][1]; k < rects[i][3]; k++) {
+            area[j][k] = 1;
+        }
+    }
+}
+
+let extent = 0;
+
+for (const v1 of area) {
+    for (const v2 of v1) {
+        if (v2 === 1) extent++;
+    }
+}
+console.log(extent);
