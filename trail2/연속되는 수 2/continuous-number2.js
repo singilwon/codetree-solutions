3505c2fb582af81d9677d1d9d4ad91d23a36e436
singilwon <164003006+singilwon@users.noreply.github.com>
2026-07-02 15:48:45 +0900

    [Wrong Answer] 연속되는 수 2 (62ms, 10MB)

---
+++ trail2/연속되는 수 2/continuous-number2.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input.slice(1, n + 1).map(Number);
+
+const max = [];
+let cnt = 1;
+
+for (let i = 0; i < n; i++) {
+    if (i === 0 || arr[i] !== arr[i - 1]) {
+        max.push(cnt);
+        cnt = 1;
+        continue;
+    }
+    cnt++;
+}
+
+console.log(Math.max(...max));
