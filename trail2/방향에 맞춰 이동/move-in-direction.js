3a19d8571c99f15062d88884abfe43fadf8cc614
singilwon <164003006+singilwon@users.noreply.github.com>
2026-07-06 15:17:50 +0900

    [Passed] 방향에 맞춰 이동 (64ms, 10MB)

---
+++ trail2/방향에 맞춰 이동/move-in-direction.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const moves = input.slice(1);
+
+let x = 0, y = 0;
+let dx = [1, 0, -1, 0], dy = [0, -1, 0, 1];
+
+for (const move of moves) {
+    const [dir, dis] = move.split(" ");
+    for (let i = 0; i < Number(dis); i++) {
+        let nx = 0;
+        let ny = 0;
+        if (dir === "S") {
+            nx = x + dx[1], ny = y + dy[1];
+        }
+        else if (dir === "E") {
+            nx = x + dx[0], ny = y + dy[0];
+        }
+        else if (dir === "W") {
+            nx = x + dx[2], ny = y + dy[2];
+        }
+        else {
+            nx = x + dx[3], ny = y + dy[3];
+        }
+        x = nx;
+        y = ny;
+    }
+}
+
+console.log(x, y);
