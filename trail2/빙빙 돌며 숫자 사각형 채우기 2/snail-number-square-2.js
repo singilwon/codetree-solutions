230385f6e243225073fa54365dcaf2c9d660e833
singilwon <164003006+singilwon@users.noreply.github.com>
2026-07-09 19:20:55 +0900

    [Passed] 빙빙 돌며 숫자 사각형 채우기 2 (67ms, 10MB)

---
+++ trail2/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+
+
+const answer = Array(n).fill(0).map(() => Array(m).fill(0));
+
+
+
+
+function inRange(x, y) {
+  return x >= 0 && x < n && y >= 0 && y < m;
+}
+
+const dx = [1, 0, -1, 0], dy = [0, 1, 0, -1];
+let x = 0, y = 0
+let dirNum = 0
+
+answer[x][y] = 1
+
+for (let i = 2; i <= n * m; i++) {
+  let nx = x + dx[dirNum], ny = y + dy[dirNum];
+
+  if (!inRange(nx, ny) || answer[nx][ny] !== 0) {
+    dirNum = (dirNum + 1) % 4;
+  }
+
+  x += dx[dirNum];
+  y += dy[dirNum];
+  answer[x][y] = i;
+}
+
+for (let i = 0; i < n; i++) {
+  let print = "";
+  for (let j = 0; j < m; j++) {
+    print += `${answer[i][j]} `;
+  }
+  console.log(print);
+}
