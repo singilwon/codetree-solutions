d7416b0eff33d0d3f211d07e44a446571f744ce0
singilwon <164003006+singilwon@users.noreply.github.com>
2026-07-10 13:25:01 +0900

    [Passed] 빙빙 돌며 사각형 채우기 (67ms, 10MB)

--- trail2/빙빙 돌며 사각형 채우기/snail-alphabet-square.js
+++ trail2/빙빙 돌며 사각형 채우기/snail-alphabet-square.js
@@ -24,7 +24,7 @@ for (let i = 1; i < n * m; i++) {
   x += dx[dirNum];
   y += dy[dirNum];
 
-  if (i + 65 >= 91) answer[x][y] = String.fromCharCode((i - 65) % 26 + 65);
+  if (i + 65 >= 91) answer[x][y] = String.fromCharCode(65 + (i % 26));
   else answer[x][y] = String.fromCharCode(i + 65);
 
 }
