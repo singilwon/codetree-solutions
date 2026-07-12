623f6f635005f3d402170861a2f3ae7b27705aee
singilwon <164003006+singilwon@users.noreply.github.com>
2026-07-12 16:41:45 +0900

    [Passed] 체크판위에서 2 (62ms, 10MB)

--- trail2/체크판위에서 2/on-the-checkboard-2.js
+++ trail2/체크판위에서 2/on-the-checkboard-2.js
@@ -4,15 +4,22 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [R, C] = input[0].split(' ').map(Number);
 const grid = input.slice(1).map(line => line.trim().split(' '));
 
-let start = grid[0][0]
+let start = grid[0][0];
+const end = grid[R - 1][C - 1];
 let answer = 0;
 
-for (let i = 0; i < R; i++) {
-    for (let j = 0; j < C; j++) {
+for (let i = 1; i < R - 1; i++) {
+    for (let j = 1; j < C - 1; j++) {
         if (grid[i][j] !== start) {
             for (let k = i + 1; k < R - 1; k++) {
                 for (let m = j + 1; m < C - 1; m++) {
-                    if (grid[k][m] === start) answer++
+                    if (
+                        grid[i][j] !== start &&
+                        grid[k][m] !== grid[i][j] &&
+                        grid[R - 1][C - 1] !== grid[k][m]
+                    ) {
+                        answer++;
+                    }
                 }
             }
         }
