eb6330af20840360a3a490a541c97b30c4f21f54
singilwon <164003006+singilwon@users.noreply.github.com>
2026-07-27 23:22:00 +0900

    [Passed] 두 가지로 열리는 자물쇠 (63ms, 10MB)

--- trail2/두 가지로 열리는 자물쇠/a-two-way-lock.js
+++ trail2/두 가지로 열리는 자물쇠/a-two-way-lock.js
@@ -1,42 +1,55 @@
 const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split('\n');
+const input = fs.readFileSync(0).toString().trim().split("\n");
 
 const n = Number(input[0]);
-const [a, b, c] = input[1].split(' ').map(Number);
-const [a2, b2, c2] = input[2].split(' ').map(Number);
+const first = input[1].split(" ").map(Number);
+const second = input[2].split(" ").map(Number);
 
-let answer = 250;
-
-const setA = new Set();
-const setB = new Set();
-const setC = new Set();
-
-let duplA = 0, duplB = 0, duplC = 0;
-
-for (let i = -2; i < 3; i++) {
-    let newA = a + i < 1 ? n + a + i : a + i > n ? a - n + i : a + i;
-    let newB = b + i < 1 ? n + b + i : b + i > n ? b - n + i : b + i;
-    let newC = c + i < 1 ? n + c + i : c + i > n ? c - n + i : c + i;
-    setA.add(newA);
-    setB.add(newB);
-    setC.add(newC);
+function wrap(num) {
+    return ((num - 1) % n + n) % n + 1;
 }
 
-for (let i = -2; i < 3; i++) {
-    let newA2 = a2 + i < 1 ? n + a2 + i : a2 + i > n ? a2 - n + i : a2 + i;
-    let newB2 = b2 + i < 1 ? n + b2 + i : b2 + i > n ? b2 - n + i : b2 + i;
-    let newC2 = c2 + i < 1 ? n + c2 + i : c2 + i > n ? c2 - n + i : c2 + i;
-    if (setA.has(newA2)) {
-        duplA++;
-    };
-    if (setB.has(newB2)) {
-        duplB++;
-    };
-    if (setC.has(newC2)) {
-        duplC++;
-    };
+function makeNearSet(center) {
+    const set = new Set();
+
+    for (let offset = -2; offset <= 2; offset++) {
+        set.add(wrap(center + offset));
+    }
+
+    return set;
 }
 
-let dupl = duplA * duplB * duplC;
+function getIntersectionSize(set1, set2) {
+    let count = 0;
 
-console.log(answer - dupl);
+    for (const value of set1) {
+        if (set2.has(value)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+const firstSets = first.map(makeNearSet);
+const secondSets = second.map(makeNearSet);
+
+const firstCount =
+    firstSets[0].size *
+    firstSets[1].size *
+    firstSets[2].size;
+
+const secondCount =
+    secondSets[0].size *
+    secondSets[1].size *
+    secondSets[2].size;
+
+const duplicateA = getIntersectionSize(firstSets[0], secondSets[0]);
+const duplicateB = getIntersectionSize(firstSets[1], secondSets[1]);
+const duplicateC = getIntersectionSize(firstSets[2], secondSets[2]);
+
+const duplicateCount = duplicateA * duplicateB * duplicateC;
+
+const answer = firstCount + secondCount - duplicateCount;
+
+console.log(answer);
