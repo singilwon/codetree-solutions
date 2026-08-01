e00fe4de2443aed121fd857db86e0f608d5df17f
singilwon <164003006+singilwon@users.noreply.github.com>
2026-08-01 13:49:20 +0900

    [Wrong Answer] 입력받은 값과 합 출력 (61ms, 10MB)

--- trail0/입력받은 값과 합 출력/output-of-value-and-sum.js
+++ trail0/입력받은 값과 합 출력/output-of-value-and-sum.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split('\n');
+let input = fs.readFileSync(0).toString().trim().split(' ');
 
 console.log(input[0], input[1], input[0] + input[1]);
