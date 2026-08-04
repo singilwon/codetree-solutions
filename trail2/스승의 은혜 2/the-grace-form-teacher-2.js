995392ee1290e706408619183f3bdb632b60830a
singilwon <164003006+singilwon@users.noreply.github.com>
2026-08-04 16:01:37 +0900

    [Passed] 스승의 은혜 2 (76ms, 14MB)

--- trail2/스승의 은혜 2/the-grace-form-teacher-2.js
+++ trail2/스승의 은혜 2/the-grace-form-teacher-2.js
@@ -4,19 +4,17 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, b] = input[0].split(' ').map(Number);
 const p = input.slice(1, 1 + n).map(Number);
 
+p.sort((a, b) => a - b);
+
 let answer = -Infinity;
 
 for (let i = 0; i < n; i++) {
-    let cost = 0;
-    let cnt = 0;
+    let cost = p[i] / 2;
+    let cnt = 1;
     for (let j = 0; j < n; j++) {
-        if (j === i) {
-            if (p[j] / 2 + cost > b) break;
-            cost += p[j] / 2;
-        } else {
-            if (p[j] + cost > b) break;
-            cost += p[j];
-        }
+        if (i === j) continue;
+        if (p[j] + cost > b) continue;
+        cost += p[j];
         cnt++;
     }
     answer = Math.max(answer, cnt);
