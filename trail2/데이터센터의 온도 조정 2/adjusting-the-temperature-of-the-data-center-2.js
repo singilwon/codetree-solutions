70ac31499aa45f47369c884a29d28434f6910302
singilwon <164003006+singilwon@users.noreply.github.com>
2026-08-08 16:16:38 +0900

    [Wrong Answer] 데이터센터의 온도 조정 2 (81ms, 14MB)

---
+++ trail2/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, c, g, h] = input[0].split(' ').map(Number);
+const ta = [], tb = [];
+for (let i = 1; i <= n; i++) {
+    const [taValue, tbValue] = input[i].split(' ').map(Number);
+    ta.push(taValue);
+    tb.push(tbValue);
+}
+
+function getScore(num, ta, tb) {
+    if (num < ta) {
+        return c;
+    } else if (num < tb) {
+        return g;
+    } else {
+        return h;
+    }
+}
+
+let answer = -Infinity;
+let max = Math.max(...tb), min = Math.min(...ta);
+
+for (let i = min; i <= max; i++) {
+    let score = 0;
+    for (let j = 0; j < n; j++) {
+        score += getScore(i, ta[j], tb[j]);
+    }
+    answer = Math.max(answer, score)
+}
+
+console.log(answer);
+
