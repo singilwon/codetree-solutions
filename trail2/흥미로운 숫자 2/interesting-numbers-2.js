426fef12328a87dcd10abd67e222d97dae5fb650
singilwon <164003006+singilwon@users.noreply.github.com>
2026-08-10 17:51:11 +0900

    [Passed] 흥미로운 숫자 2 (546ms, 36MB)

--- trail2/흥미로운 숫자 2/interesting-numbers-2.js
+++ trail2/흥미로운 숫자 2/interesting-numbers-2.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [x, y] = input[0].split(" ").map(Number);
@@ -8,6 +9,7 @@ let ans = 0;
 for (let i = x; i <= y; i++) {
     const numArr = String(i).split('').map(Number);
     const newArr = [];
+
     for (const num of numArr) {
         if (newArr.includes(num)) {
             continue;
@@ -15,7 +17,20 @@ for (let i = x; i <= y; i++) {
             newArr.push(num);
         }
     }
-    if (newArr.length === 2) ans++;
+
+    if (newArr.length === 2) {
+        let cnt1 = 0;
+        let cnt2 = 0;
+
+        for (const num of numArr) {
+            if (num === newArr[0]) cnt1++;
+            else if (num === newArr[1]) cnt2++;
+        }
+
+        if (cnt1 === 1 || cnt2 === 1) {
+            ans++;
+        }
+    }
 }
 
 console.log(ans);
