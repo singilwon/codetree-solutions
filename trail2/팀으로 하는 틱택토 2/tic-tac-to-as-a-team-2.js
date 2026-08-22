22b115d07fe279c4226c29b68aeb1ca20e238a85
singilwon <164003006+singilwon@users.noreply.github.com>
2026-08-22 22:55:06 +0900

    [Wrong Answer] 팀으로 하는 틱택토 2 (71ms, 14MB)

---
+++ trail2/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const board = input.slice(0, 3).map(line => line.split('').map(Number));
+const set = new Set();
+
+for (let i = 0; i < 3; i++) {
+    const line = board[i];
+    const team = new Set(line);
+    if (team.size === 2) {
+        const arr = Array.from(team);
+        const teamKey = `${Math.min(arr[0], arr[1])},${Math.max(arr[0], arr[1])}`;
+        set.add(teamKey);
+    }
+}
+
+for (let i = 0; i < 3; i++) {
+    const line = [];
+    for (let j = 0; j < 3; j++) {
+        line.push(board[j][i]);
+    }
+    const team = new Set(line);
+    if (team.size === 2) {
+        const arr = Array.from(team);
+        const teamKey = `${Math.min(arr[0], arr[1])},${Math.max(arr[0], arr[1])}`;
+        set.add(teamKey);
+    }
+}
+
+for (let i = 0; i < 3; i++) {
+    const line = [];
+    for (let j = 2; j >= 0; j--) {
+        line.push(board[i][j]);
+    }
+    const team = new Set(line);
+    if (team.size === 2) {
+        const arr = Array.from(team);
+        const teamKey = `${Math.min(arr[0], arr[1])},${Math.max(arr[0], arr[1])}`;
+        set.add(teamKey);
+    }
+}
+
+const line = [];
+for (let i = 0; i < 3; i++) {
+    line.push(board[i][i]);
+
+}
+const team = new Set(line);
+if (team.size === 2) {
+    const arr = Array.from(team);
+    const teamKey = `${Math.min(arr[0], arr[1])},${Math.max(arr[0], arr[1])}`;
+    set.add(teamKey);
+}
+
+console.log(set.size);
