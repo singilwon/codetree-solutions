bc7b8f709f71eacfb1d6ce0430711f4d7e4e80dc
singilwon <164003006+singilwon@users.noreply.github.com>
2026-08-22 22:58:05 +0900

    [Passed] 팀으로 하는 틱택토 2 (62ms, 10MB)

--- trail2/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
+++ trail2/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
@@ -26,22 +26,9 @@ for (let i = 0; i < 3; i++) {
     }
 }
 
-for (let i = 0; i < 3; i++) {
-    const line = [];
-    for (let j = 2; j >= 0; j--) {
-        line.push(board[i][j]);
-    }
-    const team = new Set(line);
-    if (team.size === 2) {
-        const arr = Array.from(team);
-        const teamKey = `${Math.min(arr[0], arr[1])},${Math.max(arr[0], arr[1])}`;
-        set.add(teamKey);
-    }
-}
-
 const line = [];
 for (let i = 0; i < 3; i++) {
-    line.push(board[i][i]);
+    line.push(board[i][2 - i]);
 
 }
 const team = new Set(line);
@@ -49,6 +36,18 @@ if (team.size === 2) {
     const arr = Array.from(team);
     const teamKey = `${Math.min(arr[0], arr[1])},${Math.max(arr[0], arr[1])}`;
     set.add(teamKey);
+}
+
+const line2 = [];
+for (let i = 0; i < 3; i++) {
+    line2.push(board[i][i]);
+
+}
+const team2 = new Set(line2);
+if (team2.size === 2) {
+    const arr = Array.from(team2);
+    const teamKey2 = `${Math.min(arr[0], arr[1])},${Math.max(arr[0], arr[1])}`;
+    set.add(teamKey2);
 }
 
 console.log(set.size);
