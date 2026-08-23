7b8cee06da9f6db67af00aea3f002be2cc6a616f
singilwon <164003006+singilwon@users.noreply.github.com>
2026-08-23 17:08:57 +0900

    [Passed] 독서실의 거리두기 5 (81ms, 17MB)

--- trail2/독서실의 거리두기 5/study-cafe-keeping-distance-5.js
+++ trail2/독서실의 거리두기 5/study-cafe-keeping-distance-5.js
@@ -4,40 +4,25 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const seat = input[1].split('');
 
-function isPossible(num, cnt, type) {
-    if (type === "plus") {
-        if (num + cnt >= n) {
-            return plus = n - 1;
-        } else {
-            return plus = num + cnt;
-        }
-    } else {
-        if (num - cnt < 0) {
-            return minus = 0;
-        } else {
-            return minus = num - cnt;
-        }
-    }
-}
-
 let ans = -Infinity;
 
 for (let i = 0; i < n; i++) {
     if (seat[i] === "1") continue;
-    const newSeat = seat;
+    const newSeat = [...seat];
     newSeat[i] = "1";
-    let min = -Infinity;
-    for (let j = i; j < n; j++) {
+    let min = Infinity;
+    let prev = -1;
+    for (let j = 0; j < n; j++) {
         if (newSeat[j] === "0") continue;
-        let cnt = 1;
-        while (true) {
-            let plus = isPossible(j, cnt, "plus");
-            let minus = isPossible(j, cnt, "minus");
-            if (newSeat[plus] === "1" || newSeat[minus] === "1") break;
-            cnt++;
+        if (newSeat[j] === "1") {
+            if (prev !== -1) {
+                min = Math.min(min, j - prev);
+            }
+
+            prev = j;
         }
-        min = Math.max(min, cnt - 1);
     }
+
     ans = Math.max(ans, min);
 }
 
