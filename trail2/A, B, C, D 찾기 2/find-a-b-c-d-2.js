a8215f958d3769d4e7a0cad578da885fcee04173
singilwon <164003006+singilwon@users.noreply.github.com>
2026-08-27 16:39:24 +0900

    [Passed] A, B, C, D 찾기 2 (66ms, 10MB)

--- trail2/A, B, C, D 찾기 2/find-a-b-c-d-2.js
+++ trail2/A, B, C, D 찾기 2/find-a-b-c-d-2.js
@@ -1,15 +1,53 @@
 const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split('\n');
-const numbers = input[0].split(' ').map(Number);
 
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const numbers = input[0].split(' ').map(Number);
 
 numbers.sort((a, b) => a - b);
 
-const A = numbers[0];
-const B = numbers[1];
+for (let a = 0; a < 15; a++) {
+    for (let b = a + 1; b < 15; b++) {
+        for (let c = b + 1; c < 15; c++) {
+            for (let d = c + 1; d < 15; d++) {
+                const A = numbers[a];
+                const B = numbers[b];
+                const C = numbers[c];
+                const D = numbers[d];
 
-const D = numbers[numbers.length - 4] - A - B;
+                if (!(A <= B && B <= C && C <= D)) continue;
 
-const C = numbers[numbers.length - 1] - A - B - D;
+                const arr = [
+                    A, B, C, D,
+                    A + B,
+                    B + C,
+                    C + D,
+                    D + A,
+                    A + C,
+                    B + D,
+                    A + B + C,
+                    A + B + D,
+                    A + C + D,
+                    B + C + D,
+                    A + B + C + D
+                ];
 
-console.log(A, B, C, D);
+                arr.sort((x, y) => x - y);
+
+                let same = true;
+
+                for (let i = 0; i < 15; i++) {
+                    if (arr[i] !== numbers[i]) {
+                        same = false;
+                        break;
+                    }
+                }
+
+                if (same) {
+                    console.log(A, B, C, D);
+                    return;
+                }
+            }
+        }
+    }
+}
