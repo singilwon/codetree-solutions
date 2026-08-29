e674272dbf1669168261e952c45b281cd62641b7
singilwon <164003006+singilwon@users.noreply.github.com>
2026-08-30 03:47:30 +0900

    [Wrong Answer] 특정 수 도달하기 (64ms, 10MB)

---
+++ trail1/특정 수 도달하기/reaching-specific-number.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+let arr = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let ans = 0;
+
+let index = -1;
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 250) {
+        index = i;
+        break;
+    }
+    ans += arr[i];
+}
+
+if (index === -1) {
+    console.log(ans, ans / 10)
+} else {
+    console.log(ans, ans / index);
+}
