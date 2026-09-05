df98c6cfae9b9514ce4e7131bc8a49f539ef772d
singilwon <164003006+singilwon@users.noreply.github.com>
2026-09-05 16:23:18 +0900

    [Wrong Answer] XOR 결과 최대 만들기 (67ms, 10MB)

---
+++ trail4/XOR 결과 최대 만들기/max-of-xor.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const a = input[1].split(' ').map(Number);
+
+const arr = [];
+
+let max = -Infinity;
+
+function choose(num) {
+    if (arr.length === m) {
+        let temp = 0;
+        for (const v of arr) {
+            temp ^= v;
+        }
+        max = Math.max(temp, max);
+        return;
+    }
+
+    for (let i = num; i <= n; i++) {
+        arr.push(i);
+        choose(i + 1);
+        arr.pop();
+    }
+}
+
+choose(1);
+
+console.log(max);
