7e83264374c13ce22a63d653c7803733f78f726d
singilwon <164003006+singilwon@users.noreply.github.com>
2026-09-06 15:09:48 +0900

    [Wrong Answer] 2N개 중에 N개의 정수를 적절하게 고르기 (70ms, 10MB)

---
+++ trail4/2N개 중에 N개의 정수를 적절하게 고르기/choose-n-out-of-2n-properly.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const A = input[1].split(' ').map(Number);
+
+const totalSum = A.reduce((acc, cur) => acc + cur, 0)
+
+let afterMinus = 0;
+let answer = Infinity;
+const arr = [];
+
+function choose(num) {
+    if (arr.length === n) {
+        const sum = arr.reduce((acc, cur) => acc + cur, 0);
+        afterMinus = Math.abs(sum - (totalSum - sum))
+        answer = Math.min(afterMinus, answer, sum);
+        return;
+    }
+
+    for (let i = num; i < 2 * n; i++) {
+        arr.push(A[i]);
+        choose(i + 1);
+        arr.pop();
+    }
+}
+
+choose(0);
+
+console.log(answer);
