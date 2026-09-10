d6f94ba858e232ef31d97d0975c407c6c1178904
singilwon <164003006+singilwon@users.noreply.github.com>
2026-09-10 16:21:18 +0900

    [Wrong Answer] 특정 조건에 맞게 K개 중에 1개를 N번 뽑기 (65ms, 10MB)

--- trail4/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.js
+++ trail4/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.js
@@ -5,24 +5,23 @@ const [k, n] = input[0].split(' ').map(Number);
 
 const arr = [];
 
-function choose(num) {
+function choose() {
     if (arr.length === n) {
         const selected = [];
         let possible = true;
         for (const v of arr) {
-            selected[v] = 1 || selected[v]++;
+            selected[v] = (selected[v] || 0) + 1;
         }
-        console.log(selected);
         selected.forEach((v) => { if (v >= 3) possible = false });
         if (possible) console.log(...arr);
         return;
     }
 
-    for (let i = num; i <= k; i++) {
+    for (let i = 1; i <= k; i++) {
         arr.push(i);
-        choose(i + 1);
+        choose();
         arr.pop();
     }
 }
 
-choose(1);
+choose();
